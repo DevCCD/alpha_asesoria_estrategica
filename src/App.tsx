3b6549e0d3ea81817933d845d338d4abeab07a70
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2024-03-31 21:05:42 -0500

    31.03.2024

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+import  Navbar  from './components/Navbar'; 
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <>
+    <body id="body-alpha">
+      <Navbar />
+      <Hero />
+    </body>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,33 @@
+import '../styles.css';
+
+import globe from '../assets/hero.svg'
+
+function Hero() {
+    return (
+        <>
+            <main className='main'>
+                <div className="hero-container">
+                    <img 
+                        src={globe}    
+                    />
+                    <div className="hero-text">
+                        <h1>Alpha</h1>
+                        <h4>Asesoría Estratégica</h4>
+                        <p>Generación de confianza y alta capacidad de análisis.</p>
+                        <button className="hero-btn">Escríbenos</button>
+                    </div>
+                </div>
+            </main>
+            <section>
+                <div className="container">
+                    <h1 className="display">Tasa de Interés Real</h1>
+                    <p>¿Cómo impacta actualmente y en perspectiva a la empresa y a tus inversiones personales?</p>
+                    <button className="section-btn">Escríbenos</button>
+                    <img src="https://res.cloudinary.com/ccdcloudy/image/upload/v1711922014/alpha/images/tasa_pdrrci.png" />
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Hero
