d2fedb27c5d74890ca6ba2c121a9be92c5b3bdec
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2024-06-05 12:12:59 -0500

    Repositorio Informes Reu

--- src/components/MainRepositorio.tsx
+++ src/components/MainRepositorio.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import '../styles.css';
 import '../styles/carousel.css';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 /* import '../js/carousel__informes.js'; */
 
 interface IdiomaProps {
@@ -28,63 +29,223 @@ function MainRepositorio({ idioma } : IdiomaProps) {
                     </div>
                 </div>
                 <section className='section__carousel'>
+                    <h2>Informes Mensuales</h2>
                     <div id="cCarousel">
-                        <div className="arrow" id="prev"><i className="fa-solid fa-chevron-left"></i></div>
-                        <div className="arrow" id="next"><i className="fa-solid fa-chevron-right"></i></div>
+
+                        <div className="arrow" id="prev"><FaChevronLeft/></div>
+                        <div className="arrow" id="next"><FaChevronRight/></div>
 
                         <div id="carousel-vp">
-                        <div id="cCarousel-inner">
+                            <div id="cCarousel-inner">
 
-                            <article className="cCarousel-item">
-                                <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
-                                <div className="infos">
-                                    <h3>Informe 1</h3>
-                                    <button type="button">Descargar</button>
-                                </div>
-                            </article>
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe Alpha Mayo 2024</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
 
-                            <article className="cCarousel-item">
-                                <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
-                                <div className="infos">
-                                    <h3>Informe 1</h3>
-                                    <button type="button">Descargar</button>
-                                </div>
-                            </article>
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe Alpha Abril 2024</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
 
-                            <article className="cCarousel-item">
-                                <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
-                                <div className="infos">
-                                    <h3>Informe 1</h3>
-                                    <button type="button">Descargar</button>
-                                </div>
-                            </article>
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
 
-                            <article className="cCarousel-item">
-                                <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
-                                <div className="infos">
-                                    <h3>Informe 1</h3>
-                                    <button type="button">Descargar</button>
-                                </div>
-                            </article>
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
 
-                            <article className="cCarousel-item">
-                                <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
-                                <div className="infos">
-                                    <h3>Informe 1</h3>
-                                    <button type="button">Descargar</button>
-                                </div>
-                            </article>
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
 
-                            <article className="cCarousel-item">
-                                <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
-                                <div className="infos">
-                                    <h3>Informe 1</h3>
-                                    <button type="button">Descargar</button>
-                                </div>
-                            </article>
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
 
+                            </div>
                         </div>
+                    </div>
+                </section>
+                <section className='section__carousel'>
+                    <h2>Informes Analíticos</h2>
+                    <div id="cCarousel">
+
+                        <div className="arrow" id="prev"><FaChevronLeft/></div>
+                        <div className="arrow" id="next"><FaChevronRight/></div>
+
+                        <div id="carousel-vp">
+                            <div id="cCarousel-inner">
+
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe Alpha Mayo 2024</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
+
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe Alpha Abril 2024</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
+
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
+
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
+
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
+
+                                <article className="cCarousel-item">
+                                    <img src="https://img.freepik.com/vector-gratis/fondo-vector-degradado-azul-claro-invierno_53876-128352.jpg" alt="Moon" />
+                                    <div className="infos">
+                                        <h3>Informe 1</h3>
+                                        <a>Descargar</a>
+                                    </div>
+                                </article>
+
+                            </div>
                         </div>
+                    </div>
+                </section>
+                <section className='section__carousel'>
+                    <h2>Reuniones Anuales</h2>
+                    <div className='reunion__anual'>
+
+                        <div className="card__reunion">
+                            <div className="card__calendar">
+                                <div className="top__calendar"></div>
+                                <div className="card__calendar__date">
+                                    <p>Feb</p>
+                                    <p>2024</p>
+                                </div>
+                            </div>
+                            <div className="card__info__reunion">
+                                <p>Reunión Anual (15 de feb del 2024)</p>
+                                <a href="">Ver reunión</a>
+                            </div>
+                        </div>
+
+                        <div className="card__reunion">
+                            <div className="card__calendar">
+                                <div className="top__calendar"></div>
+                                <div className="card__calendar__date">
+                                    <p>Feb</p>
+                                    <p>2024</p>
+                                </div>
+                            </div>
+                            <div className="card__info__reunion">
+                                <p>Reunión Anual (15 de feb del 2024)</p>
+                                <a href="">Ver reunión</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+                <section className='section__carousel'>
+                    <h2>Reunión con Clientes</h2>
+                    <div className='reunion__anual'>
+
+                        <div className="card__reunion">
+                            <div className="card__calendar">
+                                <div className="top__calendar"></div>
+                                <div className="card__calendar__date">
+                                    <p>Feb</p>
+                                    <p>2024</p>
+                                </div>
+                            </div>
+                            <div className="card__info__reunion">
+                                <p>Reunión Anual (15 de feb del 2024)</p>
+                                <a href="">Ver reunión</a>
+                            </div>
+                        </div>
+
+                        <div className="card__reunion">
+                            <div className="card__calendar">
+                                <div className="top__calendar"></div>
+                                <div className="card__calendar__date">
+                                    <p>Feb</p>
+                                    <p>2024</p>
+                                </div>
+                            </div>
+                            <div className="card__info__reunion">
+                                <p>Reunión Anual (15 de feb del 2024)</p>
+                                <a href="">Ver reunión</a>
+                            </div>
+                        </div>
+
+                        <div className="card__reunion">
+                            <div className="card__calendar">
+                                <div className="top__calendar"></div>
+                                <div className="card__calendar__date">
+                                    <p>Feb</p>
+                                    <p>2024</p>
+                                </div>
+                            </div>
+                            <div className="card__info__reunion">
+                                <p>Reunión Anual (15 de feb del 2024)</p>
+                                <a href="">Ver reunión</a>
+                            </div>
+                        </div>
+
+                        <div className="card__reunion">
+                            <div className="card__calendar">
+                                <div className="top__calendar"></div>
+                                <div className="card__calendar__date">
+                                    <p>Feb</p>
+                                    <p>2024</p>
+                                </div>
+                            </div>
+                            <div className="card__info__reunion">
+                                <p>Reunión Anual (15 de feb del 2024)</p>
+                                <a href="">Ver reunión</a>
+                            </div>
+                        </div>
+
                     </div>
                 </section>
             </main>
@@ -92,4 +253,4 @@ function MainRepositorio({ idioma } : IdiomaProps) {
     )
 }
 
-export default MainRepositorio
+export default MainRepositorio;
