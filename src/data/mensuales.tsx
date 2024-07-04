c5a99d71b3a18ab7acdd47e951c79336fdd6ab18
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2024-07-04 10:25:18 -0500

    repo

--- src/data/mensuales.tsx
+++ src/data/mensuales.tsx
@@ -1,12 +1,12 @@
 const mensuales = [
     {
         "_id": "0",
-        "nombre": "Informe Alpha Abril 2024",
+        "nombre": "Informe Alpha Mayo 2024",
         "url": "https://drive.google.com/file/d/14DUhE6LiyFToSfFKT9PKG_1Nsc-iQKcQ/view?usp=sharing"
     },
     {
         "_id": "1",
-        "nombre": "Informe Alpha Mayo 2024",
+        "nombre": "Informe Alpha Abril 2024",
         "url": "https://drive.google.com/file/d/1MZfBgCsNA5bTouWSmMdw_yxM4fMqvs4E/view?usp=sharing"
     },
 ]
