872fc5f8858ce1560cc5530bf2d5dbe511c25abe
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2024-07-22 14:10:59 -0500

    22.07 🗒️

--- src/components/MainContacto.tsx
+++ src/components/MainContacto.tsx
@@ -2,13 +2,27 @@ import '../styles.css';
 import redes from '../data/redes.tsx';
 import { Link } from 'react-router-dom';
 import { FaPhone } from 'react-icons/fa6';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface IdiomaProps {
     idioma: string;
 }
 
 function MainContacto ({ idioma } : IdiomaProps) {
+
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [correo, setCorreo] = useState('');
+
+    const para = "alphaasesoriaestrategica@gmail.com";
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+    
+        const mailtoLink = `mailto:${para}?subject=CV desde web Alpha: ${nombre} ${apellido} &body=Nombre: ${nombre}%0D%0AApellido: ${apellido}%0D%0ACorreo: ${correo}`;
+    
+        window.location.href = mailtoLink;
+    };
 
     useEffect(() => {
         document.title = idioma == "es" ? "Alpha | Contacto" : "Alpha | Contact"
@@ -52,22 +66,45 @@ function MainContacto ({ idioma } : IdiomaProps) {
                     <h1>{ idioma == "es" ? "¡Creemos en tu potencial, experiencia y habilidades blandas!" : "We believe in your potential, experience and soft skills!" }</h1>
                     <p>{ idioma == "es" ? "Sé parte de la institución líder del país en competitividad, con más de una década impulsando el desarrollo nacional, regional, provincial, distrital y comunal" : "Be part of the country's leading institution in competitiveness, with more than a decade promoting national, regional, provincial, district and communal development" }</p>
                     <div className="form-container">
-                        <form>
-                        <div className="input-group">
-                            <div>
-                                <label htmlFor="nombre">{ idioma == "es" ? "Nombre" : "First name" }</label>
-                                <input type="text" id="nombre" name="nombre" placeholder={ idioma == "es" ? "Nombre" : "First name" } required />
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-group">
+                                <div>
+                                    <label htmlFor="nombre">{ idioma == "es" ? "Nombre" : "First name" }</label>
+                                    <input 
+                                        type="text"
+                                        id="nombre"
+                                        name="nombre"
+                                        placeholder={ idioma == "es" ? "Nombre" : "First name" }required
+                                        value={nombre}
+                                        onChange={(e) => setNombre(e.target.value)}/>
+                                </div>
+                                <div>
+                                    <label htmlFor="apellido">{ idioma == "es" ? "Apellido" : "Last name" }</label>
+                                    <input
+                                        type="text"
+                                        id="apellido"
+                                        name="apellido"
+                                        placeholder={ idioma == "es" ? "Apellido" : "Last name" }
+                                        required
+                                        value={apellido}
+                                        onChange={(e) => setApellido(e.target.value)}/>
+                                </div>
                             </div>
-                            <div>
-                                <label htmlFor="apellido">{ idioma == "es" ? "Apellido" : "Last name" }</label>
-                                <input type="text" id="apellido" name="apellido" placeholder={ idioma == "es" ? "Apellido" : "Last name" } required />
+                            <div className="input-full">
+                                <label htmlFor="email">{ idioma == "es" ? "Correo de contacto" : "Contact email" }</label>
+                                <input
+                                    id="email"
+                                    name="email"
+                                    placeholder={ idioma == "es" ? "ejemplo@ejemplo.com" : "example@example.com" }
+                                    required 
+                                    value={correo}
+                                    onChange={(e) => setCorreo(e.target.value)}/>
                             </div>
-                        </div>
-                        <div className="input-full">
-                            <label htmlFor="email">{ idioma == "es" ? "Correo" : "Email" }</label>
-                            <input id="email" name="email" placeholder={ idioma == "es" ? "Correo" : "Email" } required />
-                        </div>
-                        <button className="hero-btn" type="submit">{ idioma == "es" ? "Enviar" : "Submit" }</button>
+                            <button 
+                                className="hero-btn"
+                                typeof="submit">
+                                    { idioma == "es" ? "Enviar" : "Submit" }
+                            </button>
                         </form>
                         <div className="message">
                             { idioma == "es" ? "No olvide adjuntar su CV en el correo*" : "Don't forget to attach your CV in the email*" }
---
+++ src/data/industria.tsx
@@ -0,0 +1,84 @@
+const industrias = [
+    {
+        "id": "0",
+        "nombre": "Minería"
+    },
+    {
+        "id": "1",
+        "nombre": "Servicios Financieros"
+    },
+    {
+        "id": "2",
+        "nombre": "Petróleo / Gas"
+    },
+    {
+        "id": "3",
+        "nombre": "Comercio"
+    },
+    {
+        "id": "4",
+        "nombre": "Alimentos"
+    },
+    {
+        "id": "5",
+        "nombre": "Energía Eléctrica"
+    },
+    {
+        "id": "6",
+        "nombre": "Telecomunicaciones"
+    },
+    {
+        "id": "7",
+        "nombre": "Bebidas / Licores"
+    },
+    {
+        "id": "8",
+        "nombre": "Multisector"
+    },
+    {
+        "id": "9",
+        "nombre": "Construcción"
+    },
+    {
+        "id": "10",
+        "nombre": "Salud"
+    },
+    {
+        "id": "11",
+        "nombre": "Agroindustria"
+    },
+    {
+        "id": "12",
+        "nombre": "Automotriz"
+    },
+    {
+        "id": "13",
+        "nombre": "Electrónica"
+    },
+    {
+        "id": "14",
+        "nombre": "Química / Fermacéutica"
+    },
+    {
+        "id": "15",
+        "nombre": "Educación"
+    },
+    {
+        "id": "16",
+        "nombre": "Turismo"
+    },
+    {
+        "id": "17",
+        "nombre": "Transporte"
+    },
+    {
+        "id": "18",
+        "nombre": "Banca"
+    },
+    {
+        "id": "19",
+        "nombre": "Otros"
+    }
+]
+
+export default industrias;
