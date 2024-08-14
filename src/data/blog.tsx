7d4bbe5b37da1d866a4af345dfd2c9b16860c4e2
Anderson Almeyda <forastero0225@gmail.com>
2024-08-14 12:49:28 -0500

    blog

--- src/data/blog.tsx
+++ src/data/blog.tsx
@@ -4,7 +4,7 @@ const blog = [
         "title" : "[OPINIÓN] Luis Carranza: “Génesis 1, 27″",
         "description" : "“Las sociedades que prosperan son aquellas que logran, dada la naturaleza humana, un contrato social donde se logra un balance entre los incentivos y la sensación de justicia”.",
         "url" : "https://peru21.pe/opinion/opinion-luis-carranza-genesis-1-27-noticia/",
-        "urlImage" : "https://peru21.pe/resizer/DLiQ-mW61OcltBZQT5t-G5NoHWg=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/RLE6LZYGQJGLBMUXY2HKOKOVZE.jpg",
+        "urlImage" : "https://res.cloudinary.com/ccdcloudy/image/upload/v1723657104/alpha/images/blog/1_genesis.jpg",
     },
     {
         "_idBlog" :"1",
